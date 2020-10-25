baf9965e7910237a6a9bdea2bbf7c24bc8b7a923
Vaas Raider <esli_1_2_3_4@hotmail.com>
2020-10-24 19:24:14 -0500

    Final

--- App9/App9/MainPage.xaml.cs
+++ App9/App9/MainPage.xaml.cs
@@ -8,11 +8,59 @@ using Xamarin.Forms;
 
 namespace App9
 {
+
     public partial class MainPage : ContentPage
     {
+        List<Persona> Personas = new List<Persona>();
+        
         public MainPage()
         {
             InitializeComponent();
+            lstPersonas.ItemsSource = Personas.ToList();
+            
         }
+
+        private void button_Clicked(object sender, EventArgs e)
+        {
+            lblMensaje.Text = txtNombre.Text;
+            Persona nuevaPersona = new Persona() { Nombre = txtNombre.Text, Correo = txtCorreo.Text };
+            Personas.Add(nuevaPersona);
+            lstPersonas.ItemsSource = Personas.OrderBy(n => n.Nombre).ToList();
+            txtNombre.Text = "";
+            txtCorreo.Text = "";
+
+        }
+
+        private void cmdLimpiar(object sender, EventArgs e)
+        {
+            Personas.RemoveAll(n => n.Nombre == txtNombre.Text);
+            lstPersonas.ItemsSource = Personas.OrderBy(n => n.Nombre).ToList();
+            txtNombre.Text = "";
+            txtCorreo.Text = "";
+        }
+
+        private void lstPersonas_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var personaSeleccionada = e.SelectedItem as Persona;
+            txtNombre.Text = personaSeleccionada.Nombre;
+            txtCorreo.Text = personaSeleccionada.Correo;
+            lblMensaje.Text = personaSeleccionada.Nombre;
+        }
+
+        private void button_modificar(object sender, EventArgs e)
+        {
+            Personas.RemoveAll(n => n.Nombre == lblMensaje.Text);
+            Persona nuevaPersona = new Persona() { Nombre = txtNombre.Text, Correo = txtCorreo.Text };
+            Personas.Add(nuevaPersona);
+            lstPersonas.ItemsSource = Personas.OrderBy(n => n.Nombre).ToList();
+            txtNombre.Text = "";
+            txtCorreo.Text = "";
+        }
+    }
+
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
     }
 }
